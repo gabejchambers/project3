59234fec29b0b3335744d2f63b4384c4daaa4350
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-10-17 22:32:21 -0400

    training pretty done, greaphs need pretty

--- project3.py
+++ project3.py
@@ -21,11 +21,12 @@ def activation(net):
     return net
 
 
-def plotLinear(data, weights):
+def plotLinear(data, weights, dataName):
     # LOBF:
     x = np.arange(-0.1, 1.1, step=0.01)
     y = weights[0] * x + weights[1]
-    plt.title("Linear Regression")
+    title = "Linear Regression " + str(dataName) + " alpha =" + str(alpha) + " #iterations = " + str(cycles)
+    plt.title(title)
     plt.xlabel("Time")
     plt.ylabel("Energy Consumption")
     plt.plot(x, y)
@@ -36,11 +37,12 @@ def plotLinear(data, weights):
     plt.show()
 
 
-def plotQuad(data, weights):
+def plotQuad(data, weights, dataName):
     # LOBF:
     x = np.arange(-0.1, 1.1, step=0.1)
     y = weights[0] * x**2 + weights[1] * x + weights[-1]
-    plt.title("Quadratic Regression")
+    title = "Quadratic Regression " + str(dataName) + " alpha =" + str(alpha) + " #iterations = " + str(cycles)
+    plt.title(title)
     plt.xlabel("Time")
     plt.ylabel("Energy Consumption")
     plt.plot(x, y)
@@ -50,11 +52,12 @@ def plotQuad(data, weights):
     plt.scatter(x,y)
     plt.show()
 
-def plotCube(data, weights):
+def plotCube(data, weights, dataName):
     # LOBF:
     x = np.arange(-0.1, 1.1, step=0.1)
     y = weights[0] * x**3 + weights[1] * x**2 + weights[2] * x + weights[-1]
-    plt.title("Quadratic Regression")
+    title = "Cubic Regression " + str(dataName) + " alpha =" + str(alpha) + " #iterations = " + str(cycles)
+    plt.title(title)
     plt.xlabel("Time")
     plt.ylabel("Energy Consumption")
     plt.plot(x, y)
@@ -65,6 +68,7 @@ def plotCube(data, weights):
     plt.show()
 
 
+# use standard alpha
 def trainLinear(matrix):
     row_num = matrix.shape[0]
     weights = np.random.uniform(-0.5, 0.5, 2) # 2 weights, 1 for bias 1 for x
@@ -78,7 +82,7 @@ def trainLinear(matrix):
             weights[-1] += learn
     return weights
 
-
+# use standard alpha
 def trainQuad(matrix):
     row_num = matrix.shape[0]
     weights = np.random.uniform(-0.5, 0.5, 3)  # 3 weights, 1 for bias, 1 for x, 1 for x^2
@@ -94,9 +98,13 @@ def trainQuad(matrix):
     return weights
 
 
+# cubic does much much better with a higher alpha, around .8 or .9, along with a high number of cycles
+# this is because the true coefficients for the cubic function are so much larger than the starting weights
+# that with a low alpha and cycle count, there isnt time to reach the true coefficients
 def trainCube(matrix):
     row_num = matrix.shape[0]
     weights = np.random.uniform(-0.5, 0.5, 4)  # 4 weights, 1 for bias, 1 for x, 1 for x^2, 1 for x^3
+    # weights = np.array([9.036, -12.0568, 3.9093, 0.1506])
     for cycle in range(cycles):
         for row in range(row_num):
             net = weights[0] * matrix[row, 0]**3 + weights[1] * matrix[row, 0]**2 + weights[2] * matrix[row, 0] + weights[-1]
@@ -116,8 +124,28 @@ def preprocess(matrix):
     return matrix
 
 
-alpha = 0.005
-cycles = 50000
+def startDataSet(dataset, dataname):
+    dataset = preprocess(dataset)
+    # print all rows of data to cross check with regression calculators:
+    # for data in dataset[:,0]:
+    #     print(data)
+    # print()
+    # for data in dataset[:,1]:
+    #     print(data)
+    w_L = trainLinear(dataset)
+    print(w_L)
+    plotLinear(dataset, w_L, dataname)
+    w_Q = trainQuad(dataset)
+    print(w_Q)
+    plotQuad(dataset, w_Q, dataname)
+    w_C = trainCube(dataset)
+    print(w_C)
+    plotCube(dataset, w_C, dataname)
+    return
+
+
+alpha = .2
+cycles = 5000
 data_1 = pd.read_csv('Project3_data/train_data_1.txt', sep=",", header=None).to_numpy()
 data_2 = pd.read_csv('Project3_data/train_data_2.txt', sep=",", header=None).to_numpy()
 data_3 = pd.read_csv('Project3_data/train_data_3.txt', sep=",", header=None).to_numpy()
@@ -129,22 +157,8 @@ data_test = pd.read_csv('Project3_data/test_data_4.txt', sep=",", header=None).t
 # print(npA[:,2]) # column 2
 # print(npA[2,1]) #value
 
-data_1 = preprocess(data_1)
-# print all rows of data to cross check with regression calculators:
-for data in data_1[:,0]:
-    print(data)
-print()
-for data in data_1[:,1]:
-    print(data)
-w_1_L = trainLinear(data_1)
-print(w_1_L)
-plotLinear(data_1, w_1_L)
-w_1_Q = trainQuad(data_1)
-print(w_1_Q)
-plotQuad(data_1, w_1_Q)
-w_1_C = trainCube(data_1)
-print(w_1_C)
-plotCube(data_1, w_1_C)
-
+startDataSet(data_1, "data_1")
+startDataSet(data_2, "data_2")
+startDataSet(data_3, "data_3")
 
 
